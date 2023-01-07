66b4f7f3e078a0241554a184a94c7ebbb5ff1526
Velhinho <goncalo.velhinho420@gmail.com>
2023-01-07 18:11:33 +0000

    refactored classnames and files

---
+++ CFGraph/StmtExprBlock.py
@@ -0,0 +1,14 @@
+from CFGraph.Block import Block
+
+class Stmt_Expr_Block(Block):
+  def __init__(self, stmt_expr, next_block) -> None:
+    self.stmt_expr = stmt_expr
+    self.next_block = next_block
+
+  def taint_analysis(self, env):
+    self.stmt_expr.eval(env)
+    self.next_block.taint_analysis(env)
+
+  def print(self):
+    print(self.stmt_expr)
+    self.next_block.print()
